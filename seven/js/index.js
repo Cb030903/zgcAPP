d84a0b0f4b3be6156d3f8ba1c5ecbf5aab858c85
陈豹 <chenbao@163.com>
2017-12-28 16:11:33 +0800

    app

--- seven/js/index.js
+++ seven/js/index.js
@@ -1,5 +1,6 @@
 ﻿$(function() {
-$('.row').mainchi()
+
+	$('.row').mainchi()
 
 	if(navigator.userAgent.indexOf('Android') > -1 || navigator.userAgent.indexOf('Adr') > -1) {
 
@@ -12,11 +13,11 @@ $('.row').mainchi()
 		var sia = sessionStorage.getItem("num");
 		var ind = $('.indexmi');
 
-		if(sia == 0 || sia == 1) {
+		if(sia <= 1) {
 			ind.eq(0).show().siblings().hide();
 			$('.in-box').eq(sia).show().siblings().hide();
 
-		} else if(sia == 2 || sia == 3) {
+		} else {
 			ind.eq(1).show().siblings().hide()
 			$('.in-box-a').eq(sia - 2).show().siblings().hide();
 
@@ -27,114 +28,71 @@ $('.row').mainchi()
 	//页面跳转
 	var tex, tex1, tex2;
 	var strm = JSON.parse(sessionStorage.getItem("str"));
-	if(strm != null && strm != undefined) {
-		$('.in-cen-left p').text(strm.BODY.proceed_rate + '%')
-		$('.in-hb-tite p').text('零钱：' + strm.BODY.sof_bal + '元')
-		$('.in-header-box p').text(strm.BODY.home_word)
-	}
-	var bodydata = JSON.parse(sessionStorage.getItem('bodydata'));
-	if(bodydata != null && bodydata.length > 0) {
-		$('.countul').empty();
-		for(var i = 0; i < bodydata.length; i++)
-			$('<li><h3>存款金额</h3><div><h4 class="countnum">' + bodydata[i].deposit_money + '</h4><p>已存<span class="daynum">' + bodydata[i].deposit_days + '</span>天</p></div></li>').appendTo('.countul');
+		if(strm != null) {
+		if(strm.BODY.proceed_rate != null) {
+			$('.in-cen-left p').text(strm.BODY.proceed_rate + '%')
+			$('.in-header-box p').text(strm.BODY.home_word)
+		}
+		if(strm.BODY.sof_bal != null) {
+			$('.in-hb-tite p').text('零钱：' + strm.BODY.sof_bal + '元')
 
+		}
 	}
+
+
 	//点击查看账户详情
 	$('.countul').on('click', 'li', function() {
 		sessionStorage.setItem('inde', $(this).index())
-		summer.callService(
-			"IuapExchangeNative.gotoNative", //原生服务（类名+方法名）
-			{
-				'isShowTabbar': "NO"
-			}, //参数
-			false //异步（true 同步）
-		)
+		nati.service("NO")
 
 		window.location.href = "html/accountdetails.html";
 	})
 
 	//点击立即转入
 	$('.transferbtn').click(function() {
-		summer.callService(
-			"IuapExchangeNative.gotoNative", //原生服务（类名+方法名）
-			{
-				'isShowTabbar': "YES",
+		nati.service("YES", 6)
 
-				'tab': 6
-
-			}, //参数
-			false //异步（true 同步）
-		)
 	})
 
-//点击协议
-$('.in-bot-a').click(function(){
-	$(this).toggleClass('active')
-var val=$('.number').val();
- val!==''&&!$(this).is('.active')?  $('.in-bot-b').css('background', '#4F85F3'):$('.in-bot-b').css('background', ' #c7d6f6');
-	
-})
-//点击协议
-	$('.in-bot-c,.in-cen,.in-bot-a a,.in-header-body a').click(function() {
+	//点击协议
+	$('.in-bot-a').click(function() {
+		$(this).toggleClass('active')
+		var val = $('.number').val();
+		val !== '' && !$(this).is('.active') ? $('.in-bot-b').css('background', '#4F85F3') : $('.in-bot-b').css('background', ' #c7d6f6');
 
-		summer.callService(
-			"IuapExchangeNative.gotoNative", //原生服务（类名+方法名）
-			{
-				'isShowTabbar': "NO"
-			}, //参数
-			false //异步（true 同步）
-		)
+	})
+	//点击协议
+	$('.in-bot-c,.in-cen,.in-bot-a a,.in-header-body a').click(function() {
+		nati.service("NO")
+
 	})
 	$('.in-header-box a').click(function() {
+		nati.service("YES", 1)
 
-		summer.callService(
-			"IuapExchangeNative.gotoNative", //原生服务（类名+方法名）
-			{
-				'isShowTabbar': "YES",
-
-				'tab': 1
-			}, //参数
-			false //异步（true 同步）
-		)
 	})
 
 	$('.in-bot-b').click(function() {
 		var a = $(this).closest('.row').find('.number').val();
-		if(a !== ''&&!$('.in-bot-a').is('.active')) {
+		if(a !== '' && !$('.in-bot-a').is('.active')) {
 
+			nati.service("YES", 1, '', a)
 
-		summer.callService(
-			"IuapExchangeNative.gotoNative", //原生服务（类名+方法名）
-			{
-				'isShowTabbar': "YES",
-
-				'transtype': a,
-				'tab': 1
-			}, //参数
-			false //异步（true 同步）
-		)
-	
 		}
 	})
 	$('.in-hb-tite span').click(function() {
-		summer.callService(
-			"IuapExchangeNative.gotoNative", //原生服务（类名+方法名）
-			{
-				'isShowTabbar': "YES",
-				'tab': 2
+		nati.service("YES", 2)
 
-			}, //参数
-			false //异步（true 同步）
-		)
 	})
 
-	$('.in-header-bod h2 span').click(function() {
-		$('.in-header-bod h2 span').toggleClass('active');
+
+	$('.in-header-bod h2 i').click(function() {
+
+		$('.in-header-bod h2 i').toggleClass('active');
 		var strt = JSON.parse(sessionStorage.getItem("str"));
 		var a = $('.in-header-bod span');
 		var b = $('.in-header-body-span');
 		var c = $('.in-header-body-spa');
-		!$('.in-header-bod h2 span').is('.active') ? a.text('* * * * *') && b.text('* * * ') && c.text('* * * ') : a.text(strt.BODY.balance) && b.text(strt.BODY.past_proceed) && c.text(strt.BODY.total_proceed);
+		!$('.in-header-bod h2 i').is('.active') ? a.text('* * * * *') && b.text('* * * ') && c.text('* * * ') : a.text(strt.BODY.balance) && b.text(strt.BODY.past_proceed) && c.text(strt.BODY.total_proceed);
 		tex = $(this).closest('header').find('h2 span').text();
 		tex1 = $(this).closest('header').find('.in-header-body-span').text();
 		tex2 = $(this).closest('header').find('.in-header-body-spa').text();
@@ -144,6 +102,6 @@ var val=$('.number').val();
 
 	})
 
-	parseFloat(sessionStorage.getItem("tex")).toString() !== "NaN" ? $('.in-header-bod span').text(sessionStorage.getItem("tex")) && $('.in-header-body-span').text(sessionStorage.getItem("tex1")) && $('.in-header-body-spa').text(sessionStorage.getItem("tex2")) && $('.in-header-bod h2 span').addClass('active') : sessionStorage.setItem("tex", tex) && sessionStorage.setItem("tex1", tex1) && sessionStorage.setItem("tex2", tex2) && $('.in-header-bod h2 span').removeClass('active');
+	parseFloat(sessionStorage.getItem("tex")).toString() !== "NaN" ? $('.in-header-bod span').text(sessionStorage.getItem("tex")) && $('.in-header-body-span').text(sessionStorage.getItem("tex1")) && $('.in-header-body-spa').text(sessionStorage.getItem("tex2")) && $('.in-header-bod h2 i').addClass('active') : sessionStorage.setItem("tex", tex) && sessionStorage.setItem("tex1", tex1) && sessionStorage.setItem("tex2", tex2) && $('.in-header-bod h2 i').removeClass('active');
 
 })
--- seven/js/main.js
+++ seven/js/main.js
@@ -1,99 +1,116 @@
+var nati = {
 
-window.onload = function() {
+	service: function(isShow, tab, mjso, a) {
 
-	setTimeout(function() {
-
+		var callback = !$('.in-box-a').eq(0).is(":hidden") ? accountdetails : iss;
 		summer.callService(
 			"IuapExchangeNative.gotoNative", //原生服务（类名+方法名）
 			{
-				'isShowTabbar': "YES",
-				'tab': 0,
+				'isShowTabbar': isShow,
+				'tab': tab,
+				'requestdata': mjso,
+				'transtype': a,
 				//                   'issigning': "1"//1是未登录     2登录未签约   3登录已签约    4签约账户为0
-				"callback": iss,
+				"callback": callback,
+				"error": errorback
+
 			}, //参数
 			false //异步（true 同步）
 		)
-	}, 200)
+	}
+}
 
-	//0 未登录  1 未签约 2已签约  3已签约账户为0
-	function iss(data) {
-		if(data.signingAndLogin != null) {
-			var sia = data.signingAndLogin;
-			var ind = $('.indexmi');
 
-			if(sia == 0 || sia == 1) {
-				ind.eq(0).show().siblings().hide();
-				$('.in-box').eq(sia).show().siblings().hide();
-
-			} else if(sia == 2 || sia == 3) {
-				ind.eq(1).show().siblings().hide()
-
-				$('.in-box-a').eq(sia - 2).show().siblings().hide();
-
-			}
-
-		}
-
-		if(data.signingAndLogin == 0 && $('.in-header-wu').is(":hidden")) {
-
-			sessionStorage.removeItem('bodydata')
-
-		}
-		sessionStorage.setItem("num", data.signingAndLogin)
-		var retflag = data.result_data;
-
-		if(retflag != undefined && retflag != null) {
-
-			var str = JSON.parse(retflag)
-			sessionStorage.setItem("str", JSON.stringify(str))
-			if(str != null) {
-				var a = $('.in-header-bod span');
-				var b = $('.in-header-body-span');
-				var c = $('.in-header-body-spa');
-				!$('.in-header-bod h2 span').is('.active') ? a.text('* * * * *') && b.text('* * * ') && c.text('* * * ') && $('.in-header-bod h2 span').removeClass('active') : a.text(str.BODY.balance) && b.text(str.BODY.past_proceed) && c.text(str.BODY.total_proceed) && $('.in-header-bod h2 span').addClass('active');
-				$('.in-cen-left p').text(str.BODY.proceed_rate + '%')
-				$('.in-hb-tite p').text('零钱：' + str.BODY.sof_bal + '元')
-				$('.in-header-box p').text(str.BODY.home_word)
-			}
-		}
-		if(!$('.in-box-a').eq(0).is(":hidden")) {
-
-			summer.callService(
-				"IuapExchangeNative.gotoNative", //原生服务（类名+方法名）
-				{
-					'isShowTabbar': "YES",
-					'tab': 5,
-					'requestdata': {
-						'txcode': '1900012',
-						'acc_kind': 'CNY',
-						'start_page': '1',
-						'page_conut': '10',
-						'branch_id': '90001'
-					},
-					"callback": accountdetails,
-				}, //参数
-				false //异步（true 同步）
-			)
+function iss(data) {
+	if(data.signingAndLogin != null) {
+		var sia = data.signingAndLogin;
+		var ind = $('.indexmi');
+		if(sia <= 1) {
+			ind.eq(0).show().siblings().hide();
+			$('.in-box').eq(sia).show().siblings().hide();
+		} else{
+			ind.eq(1).show().siblings().hide()
+			$('.in-box-a').eq(sia - 2).show().siblings().hide();
 		}
 
 	}
 
-	//账户详情
-	function accountdetails(data) {
-		var retflag = data.result_data;
-		if(retflag != undefined && retflag.length > 0) {
-			var str = JSON.parse(retflag);
-			var bodydata = str.BODY.UserInfoOut_list;
-			if(bodydata != null && bodydata.length > 0) {
-				$('.countul').empty();
+	if(data.signingAndLogin == 0 && $('.in-header-wu').is(":hidden")) {
 
-				for(var i = 0; i < bodydata.length; i++)
-					$('<li><h3>存款金额</h3><div><h4 class="countnum">' + bodydata[i].deposit_money + '</h4><p>已存<span class="daynum">' + bodydata[i].deposit_days + '</span>天</p></div></li>').appendTo('.countul');
+		sessionStorage.removeItem('bodydata')
 
-			}
-			sessionStorage.setItem('bodydata', JSON.stringify(bodydata))
+	}
+	sessionStorage.setItem("num", data.signingAndLogin)
+	var retflag = data.result_data;
+
+	if(retflag != undefined && retflag != null) {
+
+		var str = JSON.parse(retflag)
+		sessionStorage.setItem("str", JSON.stringify(str))
+		str = str != null ? str : null;
+		if(str.BODY.balance != null) {
+			var a = $('.in-header-bod span');
+			var b = $('.in-header-body-span');
+			var c = $('.in-header-body-spa');
+
+			!$('.in-header-bod h2 i').is('.active') ? a.text('* * * * *') && b.text('* * * ') && c.text('* * * ') && $('.in-header-bod h2 i').removeClass('active') : a.text(str.BODY.balance) && b.text(str.BODY.past_proceed) && c.text(str.BODY.total_proceed) && $('.in-header-bod h2 i').addClass('active');
+			$('.in-hb-tite p').text('零钱：' + str.BODY.sof_bal + '元')
+		}
+		if(str.BODY.proceed_rate != null) {
+			$('.in-cen-left p').text(str.BODY.proceed_rate + '%')
+			$('.in-header-box p').text(str.BODY.home_word)
+		}
+
+	}
+
+}
+
+//账户详情
+function accountdetails(data) {
+	var retflag = data.result_data;
+	if(retflag != undefined && retflag.length > 0) {
+		var str = JSON.parse(retflag);
+		var bodydata = str.BODY.UserInfoOut_list;
+		bodydata = JSON.parse(sessionStorage.getItem('bodydata')) ? JSON.parse(sessionStorage.getItem('bodydata')) : bodydata;
+		if(bodydata != null && bodydata.length > 0) {
+			$('.countul').empty();
+
+			for(var i = 0; i < bodydata.length; i++)
+				$('<li><h3>存款金额</h3><div><h4 class="countnum">' + bodydata[i].deposit_money + '</h4><p>已存<span class="daynum">' + bodydata[i].deposit_days + '</span>天</p></div></li>').appendTo('.countul');
 
 		}
+		sessionStorage.setItem('bodydata', JSON.stringify(bodydata))
+
+	}
+
+}
+
+function errorback(data) {
+	alert(data)
+}
+
+window.onload = function() {
+
+	var tru = sessionStorage.getItem('tru') ? sessionStorage.getItem('tru') : true;
+	var numt = tru == true ? 500 : 100;
+	setTimeout(function() {
+		nati.service("YES", 0)
+
+		tru = false;
+		sessionStorage.setItem('tru', tru)
+	}, numt)
+
+	//0 未登录  1 未签约 2已签约  3已签约账户为0
+
+	if(!$('.in-box-a').eq(0).is(":hidden")) {
+		var mjso = {
+			'txcode': '1900012',
+			'acc_kind': 'CNY',
+			'start_page': '1',
+			'page_conut': '10',
+			'branch_id': '90001'
+		};
+		nati.service("YES", 5, mjso, null)
 
 	}
 
